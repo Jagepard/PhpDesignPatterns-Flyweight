143224dbf4e58e4b36e3adef64efaaad270a2399
jagepard <dankorot@gmail.com>
2020-08-18 01:25:47 -0700

    update

--- src/ColorFlyweight.php
+++ src/ColorFlyweight.php
@@ -3,42 +3,23 @@
 declare(strict_types=1);
 
 /**
- * @author    : Korotkov Danila <dankorot@gmail.com>
- * @license   https://mit-license.org/ MIT
+ * @author  : Jagepard <jagepard@yandex.ru>
+ * @license https://mit-license.org/ MIT
  */
 
 namespace Structural\Flyweight;
 
 class ColorFlyweight implements FlyweightInterface
 {
-    /**
-     * @var string
-     */
-    private $element;
+    private string $element;
 
-    /**
-     * ColorFlyweight constructor.
-     * @param $element
-     */
     public function __construct(string $element)
     {
         $this->element = $element;
     }
 
-    /**
-     * @param string $color
-     * @return mixed|string
-     */
     public function render(string $color): string
     {
-        return sprintf("{element:%s; color:%s}\n", $this->getElement(), $color);
-    }
-
-    /**
-     * @return string
-     */
-    public function getElement(): string
-    {
-        return $this->element;
+        return sprintf("{element:%s; color:%s}\n", $this->element, $color);
     }
 }
